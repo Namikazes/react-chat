e3807a96553bfa31bdb1cd2d5d01cdc43e3cf2de
Namikazes <egorsitenko35@gmail.com>
2024-10-01 20:27:45 +0300

    Front(UI and UX)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Saitbar from "./components/Saitbar/Saitbar"
+import Chat from "./components/Chat/Chat"
+import Detail from "./components/Detail/Detail"
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+      <Saitbar />
+      <Chat />
+      <Detail />
+    </div>
   )
 }
 
